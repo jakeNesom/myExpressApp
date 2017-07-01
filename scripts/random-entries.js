f1b8e32075f2e7619a7e94ef648e5275e7f1fab2
Jake N <jake@Jakes-MBP.local>
2017-07-01 15:00:40 -0700

    push updates

---
+++ scripts/random-entries.js
@@ -0,0 +1,44 @@
+  
+
+  /* This Javascript Object generates mongoDB entries for my 
+  LAASVisualizer Project */
+  
+  var EntryGen = function(number, timeFrame) {
+    this.numOfEntries = number;
+    this.timeFrame = timeFrame;
+
+  }
+
+  EntryGen.prototype.nodeGen = function () {
+      return "node " + (1 + Math.floor(Math.random() * 6));
+  }
+
+  EntryGen.prototype.logTypeGen = function () {
+      var logType = ["Information", "Error", "Exception", "Trace", "Debug" ];
+      randomNum = Math.floor(Math.random() * 5);
+      return logType[randomNum];
+  }
+
+  EntryGen.prototype.clientGen = function () {
+      return "client " + (1 + Math.floor(Math.random() * 10));
+  }
+
+  EntryGen.prototype.createClient = function () {
+       var entry = {
+         'Client': this.clientGen(),
+         'Node': this.nodeGen(),
+         'Message': 'testMessage',
+         'LogType': this.logTypeGen(),
+         'Time': this.timeGen(this.timeFrame)
+       };
+       
+       return entry;       
+  }
+
+  EntryGen.prototype.timeGen = function (timeSpan) {
+      var startTime = new Date().getTime() - timeSpan;
+      return parseInt(startTime + Math.floor((Math.random() * timeSpan) + 60000));
+  }
+  
+  
+ module.exports(EntryGen);
